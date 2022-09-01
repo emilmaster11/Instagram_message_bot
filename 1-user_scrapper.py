a5f4a3242fb29bc850f89ed607ec97be17511cf8
emilmaster11 <39263062+emilmaster11@users.noreply.github.com>
2022-09-01 23:50:17 +0200

    Add files via upload

---
+++ 1-user_scrapper.py
@@ -0,0 +1,84 @@
+import time
+import hashlib
+from selenium import webdriver
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+def main(profil_name):
+    options=webdriver.ChromeOptions()
+    options.add_argument("--user-data-dir=C:\\Users\\emilb\\Desktop\\Python_projekte\\BOTS\\Chrome_Profile\\GoogleChromeProfile2")
+    options.add_argument("--profile-directory=Profile2")
+    
+    driver = uc.Chrome(executable_path=r'C:\Users\emilb\Desktop\Python_projekte\BOTS\driver\chromedriver.exe',options=options)
+    driver.get("https://www.instagram.com/"+profil_name+"/followers/")
+    
+    print("Programm wartet paar Sekunden")
+    
+    time.sleep(5)
+    user_liste_ = set()  #Hier werden die Gescrappten Daten gespeichert und vergliechen
+
+    int = 0
+    
+    ###20  * 12 (12 Pro scrollen) = 240User
+    while(int <200):
+        
+        
+        time.sleep(2)
+        try:
+            time.sleep(2)
+            list_of_people = driver.find_elements(By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9h _ab9m _ab9o _abcm"]//*//*//*//*//*//*//*')
+            
+            user_liste_hinzufügen = [user_liste_.add(person.text) for person in list_of_people]
+            user_liste_hinzufügen
+            
+            #Gucken ob die Comprehession darüber so funktionert.
+            # for person in list_of_people:
+            #     user_liste_.add(person.text)
+
+           
+
+            driver.execute_script("arguments[0].scrollIntoView(true);", list_of_people[-1])
+            int += 1
+
+            print("Programm wartet paar Sekunden")
+            
+            time.sleep(1)
+            
+        except NoSuchElementException as exception:
+            print(exception)
+
+
+    user_mit_dupps = r"C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\Oscars_user_mit_dupps.txt"
+    
+    with open(user_mit_dupps, mode="a") as file: 
+     file.write("\n".join(user_liste_))
+
+     
+def dups_löschen():
+
+    input_file_path = r'C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\Oscars_user_mit_dupps'   #Input ist mit Dups
+    output_file_path = r'C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\oscar.karim.txt'  #Output ist ohne DUps
+
+    completed_lines_hash = set()
+    output_file = open(output_file_path, 'r+')
+
+    for line in open(input_file_path, 'r'):
+        
+        hashValue = hashlib.md5(line.rstrip().encode('utf-8')).hexdigest()
+        
+        if hashValue not in completed_lines_hash:
+            output_file.write(line)
+            completed_lines_hash.add(hashValue)
+		    
+
+    output_file.close()
+
+
+if __name__ == "__main__":
+   main(profil_name="oscar.karem")
+   #dups_löschen()
---
+++ 2-message.py
@@ -0,0 +1,364 @@
+import time
+import random
+import schedule
+from random import randint 
+from selenium import webdriver
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+
+#Profil 3 ist Sunat u
+#Profil 2 ist Emil
+#Profil 5 ist Talha
+
+###Zeiten wann der Bot aktiv ist randomisieren.
+
+def sendmessages():
+    options=webdriver.ChromeOptions()
+    options.add_argument("lang=de-DE,de,en-US,en")
+    options.add_argument("--user-data-dir=C:\\Users\\emilb\\Desktop\\Python_projekte\\BOTS\\Chrome_Profile\\GoogleChromeProfile2")
+    options.add_argument("--profile-directory=Profile2")   
+    options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.58 Mobile Safari/537.36') 
+        
+    driver = uc.Chrome(executable_path=r'C:\Users\emilb\Desktop\Python_projekte\BOTS\driver\chromedriver.exe',options=options)    
+    driver.maximize_window() 
+
+    instagram = "https://www.instagram.com/"  #Variablen Initialisieren um sie für später zu nutzen.
+    name_der_txt_datei = r'C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\oscar.karim.txt'  #Der Pfad zur Datei mit den Namen in einer Liste
+    
+    von_zeit = 1   #Zeitliche verzögerungen
+    bis_zeit = 6 
+    
+    von_zeit1 = 1  #Zeitliche verzögerungen
+    bis_zeit1= 3
+
+
+    link_mit_nutzer, nutzer_name = "",""  #Der link_mit_nutzer dient als leerer link_mit_nutzer um daraus den Link mit dem User zu bilden
+    angeschrieben, wie_viele_anschreiben = [], 0
+    nachrichten = []
+
+    with open(name_der_txt_datei, 'r') as file:  #Leuten aus der Liste die Nachricht schreiben
+     text = file.readlines()
+    
+    schon_angeschrieben = open(r"C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\angeschrieben.txt", 'r') #Ist die Liste mit den schon angeschriebenen Namen und Prüfen ob schon angeschrieben habe
+
+    for user in text:
+
+     if  wie_viele_anschreiben == 19:  #Wenn 100 leute angeschrieben dann break und 
+      driver.quit()  #Den Driver quiten
+      break
+
+     link_mit_nutzer = instagram + user.strip()  #Txt in link_mit_nutzer umwandeln um die \n rauszubekommen   und jedes mal auf den Link von Insta resetten.
+     nutzer_name =user.strip()  #Falls der User Privat auf der Instapage hat, dann muss ich den Namen später über die Inbox suchen.
+     
+     if (nutzer_name+"\n") in schon_angeschrieben:  #Wenn User schon angeschrieben wurde, ein Nutzername weiter in der Liste prüfen.
+       continue
+     
+    
+     try:
+      time.sleep(randint(von_zeit,bis_zeit))   #Wartezeit bevor auf neues Profil geht
+      driver.get(link_mit_nutzer) #Auf die Seite des Profiles gehen
+     
+      try:
+       benachrichtungs_button= WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//button[@class="_a9-- _a9_1"]'))) #Falls Benachrichtunfs Feld aufploppt
+       benachrichtungs_button.click()
+        
+      except:
+       print("Kein Benachrichtungs Button .. gehe weiter\n") 
+
+
+
+     except NoSuchElementException as exception:
+      print(exception)
+      pass
+
+     try:
+      link_weiter = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9h _ab9m _ab9p  _aba4 _abag _abcm"]//*//*//*')))  #Testen ob Die seite nicht verfügbar ist dann auf gehe zurück zu Instagram clicken und continue machen.
+      link_weiter.click()
+      
+      print("Nutzer existiert nicht")
+
+      angeschrieben = [nutzer_name+"\n"]  #Der User der angeschrieben wurde.
+      with open(r'C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\angeschrieben.txt', mode="a") as file:  #Fügt der Liste den User nach jedem Durchgang der Liste hinzu fall es mal abstürz oder zu lange dauert und damit es keine Duplicate gibt.
+        file.write('\n'.join(angeschrieben))
+      
+      continue
+       
+     except :
+      print(str(wie_viele_anschreiben)+"User existiert...")
+      pass
+
+     time.sleep(randint(1,5))  #Paar Sekunden auf der Insta Seite vom User warten.
+     try:  #Nachrichen Button von der Instapage suchen, nicht Jeder user hat diese Option(Bei Privat nicht)
+      time.sleep(2)
+      button_nachrichten_finden = driver.find_element(by=By.XPATH, value='//button[@class="_acan _acap _acat"]') #Nachrichten Buttom vom User auf der Insta Page suchen.
+      button_nachrichten_finden.click()  #Nachrichen Button clicken
+      
+      print("Normalen Nachrichten Button gefunden")
+     
+     except NoSuchElementException as exception:  #Funktion für den anderen Nachrichtenweg
+        print(str(wie_viele_anschreiben)+"User hat kein Nachrichten Button auf der Instagram Page ... gehe anderen Weg .....\n")
+        
+        driver.get("https://www.instagram.com/direct/inbox/")
+        
+        try:
+         benachrichtungs_button= WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//button[@class="_a9-- _a9_1"]'))) #Falls Benachrichtunfs Feld aufploppt
+         benachrichtungs_button.click()
+        
+        except:
+         print("Kein Benachrichtungs Button .. gehe weiter\n") 
+        
+        time.sleep(randint(von_zeit,bis_zeit)) 
+        anderer_weg_button_finden= WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_aa4m _aa4p"]//*')))
+        
+        time.sleep(randint(von_zeit,bis_zeit)) 
+        anderer_weg_button_finden.click()  #Button zum suchen der User clicken
+       
+        time.sleep(randint(von_zeit,bis_zeit)) 
+        element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class=" _aa2u"]//*')))  #User suchen in der Bar
+   
+        time.sleep(randint(von_zeit,bis_zeit)) 
+        element.send_keys(nutzer_name)  #User eingeben
+        
+        time.sleep(randint(von_zeit1,bis_zeit1))   
+        cirkel_auswählen = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9o  _ab9v _abcm"]//*//*//*//*//*//*//*'))) #Cirkle auswählen
+
+        time.sleep(randint(von_zeit1,bis_zeit1)) 
+        cirkel_auswählen.click()  #Cirkel vom User auswählen
+
+        time.sleep(randint(von_zeit1,bis_zeit1)) 
+        user_weiter_clicken = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ac7b _ac7d"]//*//*')))
+
+        time.sleep(randint(von_zeit1,bis_zeit1)) 
+        user_weiter_clicken.click()  #Weiter clicken
+
+     time.sleep(randint(von_zeit,bis_zeit)) 
+     try:  #Nachrichten schreiben Funktion
+      time.sleep(5)
+      nachricht_schreiben = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9o  _abbh _abcm"]//*'))) #Warten bis Nachrichten Feld located ist
+      
+      time.sleep(randint(von_zeit1,bis_zeit1))
+      nachrichten = ["Guten Tag ","Hey "+" alles klar bei dir?"," Hey "+" wie gehts wie stehts?"]
+      nachricht_schreiben.send_keys(random.choice(nachrichten))  #Hier kommt die Nachricht rein
+
+     except NoSuchElementException as exception:
+       print(exception)
+       pass
+
+     
+     try:
+      time.sleep(randint(von_zeit1,bis_zeit1)) 
+      nachricht_senden = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9p  _abbi _abcm"]//*')))  #Findet senden button
+
+      time.sleep(randint(von_zeit1,bis_zeit1)) 
+      nachricht_senden.click()  #Schickt nachricht ab
+      
+
+     except NoSuchElementException as exception:
+        print(exception)
+        pass
+
+     try:
+      angeschrieben = [nutzer_name+'\n']  #Der User der angeschrieben wurde.
+      with open(r'C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\angeschrieben.txt', mode="a") as file:  #Fügt der Liste den User nach jedem Durchgang der Liste hinzu fall es mal abstürz oder zu lange dauert und damit es keine Duplicate gibt.
+        file.write('\n'.join(angeschrieben))
+
+     except Exception as e:
+      print(e)
+      pass
+
+     try:
+      wie_viele_anschreiben +=1  #Der Counter der abbricht, wenn ich die Anzahl der verschickten Nachrichten erreicht habe.
+
+     except Exception as e:
+      print(e)
+      pass
+
+
+def sendmessages_suna():
+    options=webdriver.ChromeOptions()
+    options.add_argument("lang=de-DE,de,en-US,en")
+    options.add_argument("--user-data-dir=C:\\Users\\emilb\\Desktop\\Python_projekte\\BOTS\\Chrome_Profile\\GoogleChromeProfile3")
+    options.add_argument("--profile-directory=Profile3")
+    options.add_argument(f'user-agent=Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.5195.58 Mobile Safari/537.36')   
+        
+    driver = uc.Chrome(executable_path=r'C:\Users\emilb\Desktop\Python_projekte\BOTS\driver\chromedriver.exe',options=options)    
+    driver.maximize_window() 
+
+    instagram = "https://www.instagram.com/"  #Variablen Initialisieren um sie für später zu nutzen.
+    name_der_txt_datei = r'C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\oscar.karim.txt'  #Der Pfad zur Datei mit den Namen in einer Liste
+    
+    von_zeit = 1   #Zeitliche verzögerungen
+    bis_zeit = 6 
+    
+    von_zeit1 = 1  #Zeitliche verzögerungen
+    bis_zeit1= 3
+
+
+    link_mit_nutzer, nutzer_name = "",""  #Der link_mit_nutzer dient als leerer link_mit_nutzer um daraus den Link mit dem User zu bilden
+    angeschrieben, wie_viele_anschreiben = [], 0
+    nachrichten = []
+
+    with open(name_der_txt_datei, 'r') as file:  #Leuten aus der Liste die Nachricht schreiben
+     text = file.readlines()
+    
+    schon_angeschrieben = open(r"C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\angeschrieben.txt", 'r') #Ist die Liste mit den schon angeschriebenen Namen und Prüfen ob schon angeschrieben habe
+
+    for user in text:
+
+     if  wie_viele_anschreiben == 19:  #Wenn 100 leute angeschrieben dann break und 
+      driver.quit()  #Den Driver quiten
+      break
+
+     link_mit_nutzer = instagram + user.strip()  #Txt in link_mit_nutzer umwandeln um die \n rauszubekommen   und jedes mal auf den Link von Insta resetten.
+     nutzer_name =user.strip()  #Falls der User Privat auf der Instapage hat, dann muss ich den Namen später über die Inbox suchen.
+     
+     if (nutzer_name+"\n") in schon_angeschrieben:  #Wenn User schon angeschrieben wurde, ein Nutzername weiter in der Liste prüfen.
+       continue
+     
+    
+     try:
+      time.sleep(randint(von_zeit,bis_zeit))   #Wartezeit bevor auf neues Profil geht
+      driver.get(link_mit_nutzer) #Auf die Seite des Profiles gehen
+     
+      try:
+       benachrichtungs_button= WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//button[@class="_a9-- _a9_1"]'))) #Falls Benachrichtunfs Feld aufploppt
+       benachrichtungs_button.click()
+        
+      except:
+       print("Kein Benachrichtungs Button .. gehe weiter\n") 
+
+
+
+     except NoSuchElementException as exception:
+      print(exception)
+      pass
+
+     try:
+      link_weiter = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9h _ab9m _ab9p  _aba4 _abag _abcm"]//*//*//*')))  #Testen ob Die seite nicht verfügbar ist dann auf gehe zurück zu Instagram clicken und continue machen.
+      link_weiter.click()
+      
+      print("Nutzer existiert nicht")
+
+      angeschrieben = [nutzer_name+"\n"]  #Der User der angeschrieben wurde.
+      with open(r'C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\angeschrieben.txt', mode="a") as file:  #Fügt der Liste den User nach jedem Durchgang der Liste hinzu fall es mal abstürz oder zu lange dauert und damit es keine Duplicate gibt.
+        file.write('\n'.join(angeschrieben))
+      
+      continue
+       
+     except :
+      print(str(wie_viele_anschreiben)+"User existiert...")
+      pass
+
+     time.sleep(randint(1,5))  #Paar Sekunden auf der Insta Seite vom User warten.
+     try:  #Nachrichen Button von der Instapage suchen, nicht Jeder user hat diese Option(Bei Privat nicht)
+      button_nachrichten_finden = driver.find_element(by=By.XPATH, value='//button[@class="_acan _acap _acat"]') #Nachrichten Buttom vom User auf der Insta Page suchen.
+      button_nachrichten_finden.click()  #Nachrichen Button clicken
+      
+      print("Normalen Nachrichten Button gefunden")
+     
+     except NoSuchElementException as exception:  #Funktion für den anderen Nachrichtenweg
+        print(str(wie_viele_anschreiben)+"User hat kein Nachrichten Button auf der Instagram Page ... gehe anderen Weg .....\n")
+        
+        driver.get("https://www.instagram.com/direct/inbox/")
+        
+        try:
+         benachrichtungs_button= WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//button[@class="_a9-- _a9_1"]'))) #Falls Benachrichtunfs Feld aufploppt
+         benachrichtungs_button.click()
+        
+        except:
+         print("Kein Benachrichtungs Button .. gehe weiter\n") 
+        
+        time.sleep(randint(von_zeit,bis_zeit)) 
+        anderer_weg_button_finden= WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_aa4m _aa4p"]//*')))
+        
+        time.sleep(randint(von_zeit,bis_zeit)) 
+        anderer_weg_button_finden.click()  #Button zum suchen der User clicken
+       
+        time.sleep(randint(von_zeit,bis_zeit)) 
+        element = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class=" _aa2u"]//*')))  #User suchen in der Bar
+   
+        time.sleep(randint(von_zeit,bis_zeit)) 
+        element.send_keys(nutzer_name)  #User eingeben
+        
+        time.sleep(randint(von_zeit1,bis_zeit1))   
+        cirkel_auswählen = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9o  _ab9v _abcm"]//*//*//*//*//*//*//*'))) #Cirkle auswählen
+
+        time.sleep(randint(von_zeit1,bis_zeit1)) 
+        cirkel_auswählen.click()  #Cirkel vom User auswählen
+
+        time.sleep(randint(von_zeit1,bis_zeit1)) 
+        user_weiter_clicken = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ac7b _ac7d"]//*//*')))
+
+        time.sleep(randint(von_zeit1,bis_zeit1)) 
+        user_weiter_clicken.click()  #Weiter clicken
+
+     time.sleep(randint(von_zeit,bis_zeit)) 
+
+     try:  #Nachrichten schreiben Funktion
+      nachricht_schreiben = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9o  _abbh _abcm"]//*'))) #Warten bis Nachrichten Feld located ist
+      
+      time.sleep(randint(von_zeit1,bis_zeit1))
+      nachrichten = ["Hey :) ich hab gesehen, du folgst auch dem Oskar Karem. Ich persönlich finde die Themen Businessgründung und entwicklung super spannend. Sind diese Themen auch für dich relevant?","Hi, \ncooles Profil :) Ich hab gesehen du folgst auch dem Oscar Karem, Ich persönlich finde die Themen Businessgründung und entwicklung super spannend. Sind diese Themen auch für dich relevant? "]
+      nachricht_schreiben.send_keys(random.choice(nachrichten))  #Hier kommt die Nachricht rein
+
+     except NoSuchElementException as exception:
+       print(exception)
+       pass
+
+     
+     try:
+      time.sleep(randint(von_zeit1,bis_zeit1)) 
+      nachricht_senden = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="_ab8w  _ab94 _ab99 _ab9f _ab9m _ab9p  _abbi _abcm"]//*')))  #Findet senden button
+
+      time.sleep(randint(von_zeit1,bis_zeit1)) 
+      nachricht_senden.click()  #Schickt nachricht ab
+      
+
+     except NoSuchElementException as exception:
+        print(exception)
+        pass
+
+     try:
+      angeschrieben = [nutzer_name+'\n']  #Der User der angeschrieben wurde.
+      with open(r'C:\Users\emilb\Desktop\Business_mit_Nic\Insta_Bot\Oscar_Karem\angeschrieben.txt', mode="a") as file:  #Fügt der Liste den User nach jedem Durchgang der Liste hinzu fall es mal abstürz oder zu lange dauert und damit es keine Duplicate gibt.
+        file.write('\n'.join(angeschrieben))
+
+     except Exception as e:
+      print(e)
+      pass
+
+     try:
+      wie_viele_anschreiben +=1  #Der Counter der abbricht, wenn ich die Anzahl der verschickten Nachrichten erreicht habe.
+
+     except Exception as e:
+      print(e)
+      pass
+     
+   
+if __name__ == "__main__":
+
+   sendmessages_suna() 
+   time.sleep(100)
+   sendmessages()
+   time.sleep(300) 
+
+   sendmessages() 
+   time.sleep(100)
+   sendmessages_suna()
+   time.sleep(300) 
+
+   sendmessages() 
+   time.sleep(100)
+   sendmessages_suna()
+   time.sleep(300) 
+   ###3.Auf einem Server alles laufen lassen
+   
+  
+
+
+
+    
